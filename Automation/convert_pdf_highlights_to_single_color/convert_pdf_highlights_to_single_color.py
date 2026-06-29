81d50c8197d3f3ad4f56f6facced8297ad144827
Abdul Rehman <abdulrehman229971@gmail.com>
2026-06-29 16:08:55 +0500

    Add annotations overlap detection

--- Automation/convert_pdf_highlights_to_single_color/convert_pdf_highlights_to_single_color.py
+++ Automation/convert_pdf_highlights_to_single_color/convert_pdf_highlights_to_single_color.py
@@ -7,4 +7,10 @@ for page in doc:
         annote.set_colors(stroke=(1, 0, 0))
         annote.update()
 
+
+def is_rectangles_overlap(rect1, rect2):
+    if rect1.x0 >= rect2.x1 or rect1.x1 <= rect2.x0 or rect1.y0 >= rect2.y1 or rect1.y1 <= rect2.y0:
+        return False
+    return True
+
 doc.save("output.pdf")
