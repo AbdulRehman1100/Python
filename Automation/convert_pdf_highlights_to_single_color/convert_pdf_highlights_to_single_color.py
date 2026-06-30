70605ba1eb8dc460469e59098e7095438d91b54c
Abdul Rehman <abdulrehman229971@gmail.com>
2026-06-30 14:20:48 +0500

    Add merg_pass function for single overlap merge per pass

--- Automation/convert_pdf_highlights_to_single_color/convert_pdf_highlights_to_single_color.py
+++ Automation/convert_pdf_highlights_to_single_color/convert_pdf_highlights_to_single_color.py
@@ -13,11 +13,35 @@ def merge_rectangles(rect1, rect2):
     new_y1 = max(rect1.y1, rect2.y1)
     return pymupdf.Rect(new_x0, new_y0, new_x1, new_y1)
 
-if __name__ == "__main__":
-    doc = pymupdf.open("git_cheat.pdf")
-    for page in doc:
-        for annote in page.annots():
-            annote.set_colors(stroke=(1, 0, 0))
-            annote.update()
+def merg_pass(rectangles):
+    result = []
+    
+    for rect in rectangles:
+        merged_flag = False
+        overlap_index = None
+        
+        for i, result_rect in enumerate(result):
+            if is_rectangles_overlap(rect, result_rect):
+                overlap_index = i
+                merged_flag = True
+                break
+        
+        if merged_flag:
+           result.append(merge_rectangles(rect, result[overlap_index]))
+           result.pop(overlap_index)
+        else:
+            result.append(rect)
+    
+    return result
 
-    doc.save("output.pdf")
+# if __name__ == "__main__":
+#     doc = pymupdf.open("git_cheat.pdf")
+#     for page in doc:
+#         for annote in page.annots():
+#             annote.set_colors(stroke=(1, 0, 0))
+#             annote.update()
+
+#     doc.save("output.pdf")
+
+test_rects = [pymupdf.Rect(0,0,50,50), pymupdf.Rect(25,25,75,75), pymupdf.Rect(200,200,300,300)]
+print(merg_pass(test_rects))
