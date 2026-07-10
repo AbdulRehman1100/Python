b8f792cb68102a195ff99fe974181b7f4dcbb547
Abdul Rehman <abdulrehman229971@gmail.com>
2026-07-10 17:31:53 +0500

    Add Node class

---
+++ Data_Structures/Linked_List/linked_list.py
@@ -0,0 +1,21 @@
+
+class Node:
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+
+    @property
+    def data(self):
+        return self._data
+    
+    @data.setter
+    def data(self, data):
+        self._data = data
+    
+    @property
+    def next(self):
+        return self._next
+    
+    @next.setter
+    def next(self, next):
+        self._next = next
