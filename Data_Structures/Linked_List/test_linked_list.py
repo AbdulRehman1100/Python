ea4842ed4e4e2f67f1dc4411f936adec6f27db47
Abdul Rehman <abdulrehman229971@gmail.com>
2026-07-19 16:20:53 +0500

    Add test for linked_list start and move methods

--- Data_Structures/Linked_List/test_linked_list.py
+++ Data_Structures/Linked_List/test_linked_list.py
@@ -44,3 +44,43 @@ def test_str_multiple_elements():
     ll.add(2)
     ll.add(3)
     assert str(ll) == "1 -> 2 -> 3"
+
+def test_start_empty_list():
+    ll = LinkedList()
+    ll.start()
+    assert ll.current is None
+
+def test_start_non_empty_list():
+    ll = LinkedList()
+    ll.add(1)
+    ll.add(2)
+    ll.add(3)
+    ll.start()
+    assert ll.current.data == 1
+
+def test_move_success():
+    ll = LinkedList()
+    ll.add(1)
+    ll.add(2)
+    ll.add(3)
+    ll.start()
+    
+    result = ll.move()
+    assert result == True
+    assert ll.current.data == 2
+
+def test_move_at_end():
+    ll = LinkedList()
+    ll.add(1)
+    ll.start()
+    
+    result = ll.move()
+    assert result == False
+    assert ll.current.data == 1
+
+def test_move_empty_list():
+    ll = LinkedList()
+    result = ll.move()
+    
+    assert result == False
+    assert ll.current is None
