23d1c07371446c6ab1b22dfefddd28c5ad338834
Abdul Rehman <abdulrehman229971@gmail.com>
2026-07-29 17:47:55 +0500

    Add search_min() and delete() methods

--- Data_Structures/Binary_Search_Tree/BST.py
+++ Data_Structures/Binary_Search_Tree/BST.py
@@ -94,3 +94,70 @@ class BST:
             success = True
             
         return success
+
+    def search_min(self):
+        return self._search_min_helper(self._root).object
+
+    def _search_min_helper(self, node):
+        if node.left is None:
+            return node
+        return self._search_min_helper(node.left)
+
+    def delete(self, object):
+        self._root, success = self._delete_helper(self._root, object)
+        return success
+
+    def _delete_helper(self, node, object):
+        # base case where match is not in the BST
+        if node is None:
+            return (node, False)
+        # traverse respective branch to found match
+        if object < node.object:
+            node.left, success = self._delete_helper(node.left, object)
+        elif object > node.object:
+            node.right, success = self._delete_helper(node.right, object)
+        # handle the case if the node to be deleted have both child
+        elif node.left is not None and node.right is not None:
+            min_node = self._search_min_helper(node.right)
+            node.object = min_node.object
+            node.right, success = self._delete_helper(node.right, min_node.object)
+        else:
+            # handle the case if the node to be deleted have only one child
+            if node.left is None:
+                node = node.right
+            elif node.right is None:
+                node = node.left
+            # handle the case if the node to be deleted have no child
+            else:
+                node = None
+            success = True
+
+        return (node, success)
+
+t = BST()
+t.insert(50)
+t.insert(30)
+t.insert(70)
+t.insert(60)
+t.insert(80)
+t.delete(50)  # root ke dono children hain
+print(t.inorder())
+
+t2 = BST()
+t2.insert(50)
+t2.insert(30)
+t2.insert(70)
+t2.delete(30)
+print(t2.inorder())  # expect: [50, 70]
+
+t3 = BST()
+t3.insert(50)
+t3.insert(30)
+t3.insert(20)
+t3.delete(30)
+print(t3.inorder())  # expect: [20, 50]
+
+t5 = BST()
+t5.insert(50)
+t5.delete(50)
+print(t5._root)  # expect None
