5a86da1346a9a050b208e92e55dbb8b466bac23e
Abdul Rehman <abdulrehman229971@gmail.com>
2026-08-06 20:52:30 +0500

    Change Max_level from class variable to instance variable and add __str__ and get_level_0_items for SkipList

--- Data_Structures/Skip_List/SkipList.py
+++ Data_Structures/Skip_List/SkipList.py
@@ -23,15 +23,19 @@ class Node:
         return len(self._next)
 
 class SkipList:
-    Max_level = 30
-    def __init__(self, min, max):
-        self._head = Node(SkipList.Max_level, min)
-        self._tail = Node(SkipList.Max_level, max)
+    DEFAULT_MAX_LEVEL = 30
+    def __init__(self, min_value, max_value, max_level = None):
+        self._max_level = (
+            max_level if max_level is not None
+            else SkipList.DEFAULT_MAX_LEVEL
+        )
+        self._head = Node(self._max_level, min_value)
+        self._tail = Node(self._max_level, max_value)
 
         for level in range(len(self._head)):
             self._head[level] = self._tail
 
-    def __search_predecessor(self, data):
+    def _search_predecessor(self, data):
         predecessors = [None] * len(self._head)
         current = self._head
 
@@ -42,20 +46,45 @@ class SkipList:
 
         return predecessors
 
-    def __random_level(self):
+    def _random_level(self):
         level = 0
         result = random.choice((0,1))
-        while result == 1 and level < SkipList.Max_level:
+        while result == 1 and level < self._max_level:
             level += 1
             result = random.choice((0,1))
 
         return level
 
     def insert(self, data):
-        update = self.__search_predecessor(data)
-        node_height = self.__random_level()
+        update = self._search_predecessor(data)
+        node_height = self._random_level()
         new_node = Node(node_height, data)
 
         for level in range(node_height + 1):
             new_node[level] = update[level][level]
             update[level][level] = new_node
+
+    def __str__(self):
+        lines = []
+
+        for level in range(self._max_level - 1, -1, -1):
+            current = self._head
+            elements = []
+            while current[level].data != self._tail.data:
+                current = current[level]
+                elements.append(str(current.data))
+            if elements:  # sirf woh levels dikhao jinme koi element ho
+                lines.append(f"Level {level}: " + " -> ".join(elements))
+
+        return "\n".join(lines)
+
+    def get_level_0_items(self):
+        LEVEL_0 = 0
+        elements = []
+
+        current = self._head
+        while current[LEVEL_0].data != self._tail.data:
+            current = current[LEVEL_0]
+            elements.append(current.data)
+
+        return elements
