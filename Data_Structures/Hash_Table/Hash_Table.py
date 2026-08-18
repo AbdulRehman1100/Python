24b0f9060d8f17675b846c6d7367fa379f13fac0
Abdul Rehman <abdulrehman229971@gmail.com>
2026-08-18 17:52:03 +0500

    Fix HashTable to update duplicate keys, fix table initialization to create distinct Node objects per bucket

--- Data_Structures/Hash_Table/Hash_Table.py
+++ Data_Structures/Hash_Table/Hash_Table.py
@@ -34,11 +34,16 @@ class Node:
 class HashTable:
     def __init__(self, size):
         self._table_size = size if is_prime(size) else next_prime(size)
-        self._table = [Node()] * self._table_size
+        self._table = [Node() for _ in range(self._table_size)] # dummy header node(key=None, data=None) list
 
     def add(self, key, data):
         index = hash(key) % self._table_size
-        print(index)
-        new_node = Node(key, data)
-        new_node.next = self._table[index]
-        self._table[index] = new_node
+
+        node = self._table[index]
+        while node.key != key and node.next is not None: # traverse to either matching key node or end of the list
+           node = node.next
+
+        if node.key == key: # update data of duplicate key
+            node.data = data
+        else:
+            node.next = Node(key, data) # append new key, data pair at end
