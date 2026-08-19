785069323ff220c37e99d3f5846cc1f47498df1c
Abdul Rehman <abdulrehman229971@gmail.com>
2026-08-19 20:25:57 +0500

    Add input validation to HashTable methods

--- Data_Structures/Hash_Table/Hash_Table.py
+++ Data_Structures/Hash_Table/Hash_Table.py
@@ -33,12 +33,19 @@ class Node:
 
 class HashTable:
     def __init__(self, size):
+        if type(size) != int:
+            raise TypeError("Table size must be an integer")
+        if size < 1:
+            raise ValueError("Table size can't be non positive")
+        
         self._table_size = size if is_prime(size) else next_prime(size)
         self._table = [Node() for _ in range(self._table_size)] # dummy header node(key=None, data=None) list
 
     def add(self, key, data):
+        if key is None:
+            raise TypeError("Key can't be None")
+        
         index = hash(key) % self._table_size
-
         node = self._table[index]
         while node.key != key and node.next is not None: # traverse to either matching key node or end of the list
            node = node.next
@@ -49,6 +56,9 @@ class HashTable:
             node.next = Node(key, data) # append new key, data pair at end
 
     def get(self, key):
+        if key is None:
+            raise TypeError("Key can't be None")
+        
         index = hash(key) % self._table_size
         node = self._table[index]
         while node is not None:
@@ -58,8 +68,10 @@ class HashTable:
         return None
 
     def remove(self, key):
+        if key is None:
+            raise TypeError("Key can't be None")
+        
         index = hash(key) % self._table_size
-
         node = self._table[index]
         prev_node = node # faciliate the delete
         while node.key != key and node.next is not None: # traverse to either matching key node or end of the list
